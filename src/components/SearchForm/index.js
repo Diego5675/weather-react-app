1d783cbe2f3a0c8359545ca5980472596cbfefbf
Diego Pascual <barcadiego1175@gmail.com>
2021-08-31 10:33:08 -0500

    Add style to form

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./SearchForm.module.css";
 
 const SearchForm = ({ onSubmit }) => {
   const [value, setValue] = useState("");
@@ -14,15 +15,23 @@ const SearchForm = ({ onSubmit }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Search location"
-        value={value}
-        onChange={handleChange}
-      />
-      <button>Search</button>
-    </form>
+    <div className={styles.formContainer}>
+      <div className={styles.inputContainer}>
+        <span className={`${styles.searchIcon} material-icons`}>search</span>
+        <form className={styles.formSearch} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Search location"
+            value={value}
+            className={styles.input}
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      <button className={styles.searchButton} onClick={handleSubmit}>
+        Search
+      </button>
+    </div>
   );
 };
 
